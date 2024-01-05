beace887ffe46b80839ca727af83bd168e7b8b55
rob040 <rob040@hotmail.nl>
2024-01-06 00:18:34 +0100

    enhance PIO CI script, to report build errors

--- pio_ci_ex_ALL.bat
+++ pio_ci_ex_ALL.bat
@@ -1,5 +1,6 @@
 @echo off
 rem Build all listed examples of this library on PIO CLI; may take some time
+rem Verifies that library and all examples still compile
 
 setlocal EnableDelayedExpansion enableextensions
 set CI_DIR=%~dp0\..\async-esp-fs-webserver.pio-ci
@@ -10,6 +11,7 @@ FOR  %%E IN (%EXAMPLES%) DO IF NOT EXIST %CI_DIR%\ci_ex_%%E MKDIR %CI_DIR%\ci_ex
 @echo on
 FOR  %%E IN (%EXAMPLES%) DO (
  pio ci  -c platformio.ini --board=esp32dev --keep-build-dir --build-dir=%CI_DIR%\ci_ex_%%E --lib=. .\examples\%%E\*.* > %CI_DIR%\ci_ex_%%E\build.out.txt 2>%CI_DIR%\ci_ex_%%E\build.err.txt
+ @for /f %%i in ("%CI_DIR%\ci_ex_%%E\build.err.txt") do @set size=%%~zi && @echo ###ERROR in %%i && @type %%i
  )
 @echo off
 :: type %CI_DIR%/ci_ex_%%E/build.err.txt
