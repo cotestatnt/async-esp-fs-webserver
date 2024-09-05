19260f4b285f6b014749387ffcd4d556f02ead3a
Grégory Janiszewski <gregory@ascolab.fr>
2024-09-05 14:52:35 +0200

    Fix wrong comparison test in addOption
    It means the test is mainly false, then the config file is rewritten at each startup !

--- src/SetupConfig.hpp
+++ src/SetupConfig.hpp
@@ -231,7 +231,7 @@ class SetupConfigurator
                 key += numOptions ;
 
             // If key is present and value is the same, we don't need to create/update it.
-            if (doc.containsKey(key.c_str()) && doc[key] != val)
+            if (doc.containsKey(key.c_str()) && doc[key] == val)
                 return;
 
             // if min, max, step != from default, treat this as object in order to set other properties
