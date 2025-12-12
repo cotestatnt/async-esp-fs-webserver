63020f3c9b4922ab2077afa016ef317eed1c7219
Tolentino Cotesta <cotestatnt@yahoo.com>
2025-12-12 09:30:06 +0100

    fix examples

--- examples/simpleServer/simpleServer.ino
+++ examples/simpleServer/simpleServer.ino
@@ -14,12 +14,11 @@ const uint8_t ledPin = LED_BUILTIN;
 
 ////////////////////////////////  Filesystem  /////////////////////////////////////////
 bool startFilesystem() {
-  if (LittleFS.begin()){
+  if (LittleFS.begin()) {
     server.printFileList(LittleFS, "/", 1);
     Serial.println();
     return true;
-  }
-  else {
+  } else {
     Serial.println("ERROR on mounting filesystem. It will be reformatted!");
     LittleFS.format();
     ESP.restart();
@@ -28,22 +27,14 @@ bool startFilesystem() {
 }
 
 ////////////////////////////  Load custom options //////////////////////////////////////
-bool loadApplicationConfig() {    
-    File config = server.getConfigFile("r");
-    if (config) {
-        using namespace AsyncFSWebServer;
-        Json doc;
-        String content = "";
-        while (config.available()) {
-            content += (char)config.read();
-        }
-        bool parsed = doc.parse(content);
-        if (parsed) {            
-            doc.getNumber("Test int variable", testInt);
-            doc.getNumber("Test float variable", testFloat);
-        }
-    }
+bool loadApplicationConfig() {
+  if (LittleFS.exists(server.getConfiFileName())) {
+    // Test "options" values
+    server.getOptionValue("Test int variable", testInt);
+    server.getOptionValue("Test float variable", testFloat);
     return true;
+  }
+  return false;
 }
 
 
@@ -53,18 +44,18 @@ bool loadApplicationConfig() {
 */
 #ifdef ESP32
 void getFsInfo(fsInfo_t* fsInfo) {
-	fsInfo->fsName = "LittleFS";
-	fsInfo->totalBytes = LittleFS.totalBytes();
-	fsInfo->usedBytes = LittleFS.usedBytes();  
+  fsInfo->fsName = "LittleFS";
+  fsInfo->totalBytes = LittleFS.totalBytes();
+  fsInfo->usedBytes = LittleFS.usedBytes();
 }
 #endif
 
 
 //---------------------------------------
-void handleLed(AsyncWebServerRequest *request) {
+void handleLed(AsyncWebServerRequest* request) {
   static int value = false;
   // http://xxx.xxx.xxx.xxx/led?val=1
-  if(request->hasParam("val")) {
+  if (request->hasParam("val")) {
     value = request->arg("val").toInt();
     digitalWrite(ledPin, value);
   }
@@ -75,55 +66,51 @@ void handleLed(AsyncWebServerRequest *request) {
 
 
 void setup() {
-    pinMode(ledPin, OUTPUT);
-    Serial.begin(115200);
-    delay(1000);
-    if (startFilesystem()) {        
-        // Load application config options
-        if (loadApplicationConfig()) {        
-            Serial.printf("Stored \"testInt\" value: %d\n", testInt);
-            Serial.printf("Stored \"testFloat\" value: %3.3f\n", testFloat);
-        }
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(115200);
+  delay(1000);
+  if (startFilesystem()) {
+    // Load application config options
+    if (loadApplicationConfig()) {
+      Serial.printf("Stored \"testInt\" value: %d\n", testInt);
+      Serial.printf("Stored \"testFloat\" value: %3.3f\n", testFloat);
     }
-    else
-        Serial.println("LittleFS error!");
+  } else
+    Serial.println("LittleFS error!");
 
-	// Try to connect to WiFi (will start AP if not connected after timeout)
-    if (!server.startWiFi(10000)) {
-        Serial.println("\nWiFi not connected! Starting AP mode...");
-        server.startCaptivePortal("ESP_AP", "123456789", "/setup");
-    }
+  // Try to connect to WiFi (will start AP if not connected after timeout)
+  if (!server.startWiFi(10000)) {
+    Serial.println("\nWiFi not connected! Starting AP mode...");
+    server.startCaptivePortal("ESP_AP", "123456789", "/setup");
+  }
 
-    // Add custom application options tab and set custom title
-    server.addOptionBox("Custom options");
-    server.addOption("Test int variable", testInt);
-    server.addOption("Test float variable", (double)testFloat, 0.0, 100.0, 0.001);
-    server.setSetupPageTitle("Simple Async ESP FS WebServer");
+  // Add custom application options tab and set custom title
+  server.addOptionBox("Custom options");
+  server.addOption("Test int variable", testInt);
+  server.addOption("Test float variable", (double)testFloat, 0.0, 100.0, 0.001);
+  server.setSetupPageTitle("Simple Async ESP FS WebServer");
 
-    // Enable ACE FS file web editor and add FS info callback function    
+  // Enable ACE FS file web editor and add FS info callback function
 #ifdef ESP32
-    server.enableFsCodeEditor(getFsInfo);
+  server.enableFsCodeEditor(getFsInfo);
 #else
-    server.enableFsCodeEditor();
+  server.enableFsCodeEditor();
 #endif
 
-    // Custom endpoint handler
-    server.on("/led", HTTP_GET, handleLed);
+  // Custom endpoint handler
+  server.on("/led", HTTP_GET, handleLed);
 
-    // Start server
-    server.init();
-    Serial.print(F("\nAsync ESP Web Server started on IP Address: "));
-    Serial.println(server.getServerIP());
-    Serial.println(F(
-        "This is \"simpleServer.ino\" example.\n"
-        "Open /setup page to configure optional parameters.\n"
-        "Open /edit page to view, edit or upload example or your custom webserver source files."
-    ));
-
+  // Start server
+  server.init();
+  Serial.print(F("\nAsync ESP Web Server started on IP Address: "));
+  Serial.println(server.getServerIP());
+  Serial.println(F(
+    "This is \"simpleServer.ino\" example.\n"
+    "Open /setup page to configure optional parameters.\n"
+    "Open /edit page to view, edit or upload example or your custom webserver source files."));
 }
 
 void loop() {
-    // This delay is required in order to avoid loopTask() WDT reset on ESP32
-    delay(10);  
-
+  // This delay is required in order to avoid loopTask() WDT reset on ESP32
+  delay(10);
 }
